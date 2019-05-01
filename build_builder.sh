7101d5902d99650d924910491ffee0668e9dfb94
mammix2 <mammix2@hotmail.com>
2019-05-01 18:48:26 +0100

    error output

--- build_builder.sh
+++ build_builder.sh
@@ -20,7 +20,7 @@ function fStartInfo() {
 	echo -e " "
 }
 function fName_Error() {
-	echo -e "Missing \e[1m<projectname>\e[0m parameter. make sure project Dockerfile exists in path 'project/<projectname>/Dockerfile_<target platform>'"
+	echo -e "Missing \e[1m<projectname>\e[0m parameter. Make sure project Dockerfile_xxx exists in path 'project/<projectname>/Dockerfile_<target platform>' e.g. Dockerfile_win [osx, win, linux]"
 	echo -e "Options: ./build_builder.sh \e[1m<projectname>\e[0m <target platform>"
 	echo -e "Usage: ./build_builder.sh \e[1mboostcoin\e[0m win"
 }
